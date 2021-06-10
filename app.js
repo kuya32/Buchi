7050e757c56dc4fa7a3e9f0fc797f0a258b592c9
Marchael Acode <m.acode@outlook.com>
2021-06-10 12:57:45 -0700

    Trying out for errors

--- app.js
+++ app.js
@@ -2,22 +2,23 @@
 
 const express = require('express');
 const app = express();
+var document;
 
 app.get("/", function(req, res) {
   res.send("You are live!")
 });
 
-window.document.querySelector(".reveal-content").addEventListener("submit", submitForm);
+document.querySelector(".reveal-content").addEventListener("submit", submitForm);
 
 function submitForm(e) {
   e.preventDefault()
 
-  let name = window.document.querySelector(".form-control#name").value;
-  let email = window.document.querySelector(".form-control#email").value;
-  let subject = window.document.querySelector(".form-control#subject").value;
-  let message = window.document.querySelector(".form-control#message").value;
+  let name = document.querySelector(".form-control#name").value;
+  let email = document.querySelector(".form-control#email").value;
+  let subject = document.querySelector(".form-control#subject").value;
+  let message = document.querySelector(".form-control#message").value;
 
-  window.document.querySelector(".reveal-content").reset();
+  document.querySelector(".reveal-content").reset();
 
   sendEmail(name, email, subject, message)
 }
